517b443be6ca06986fd108731cf3067f6e09cc32
Angie Kassab <anasab12@gmail.com>
2018-07-03 16:07:19 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,7 @@ end
 
 def remove_element_from_end_of_array(array)
   emotions = ["happy","happy","joy","joy"]
-  emotions = pop[3]
+  emotions.pop
 end
 
 def remove_element_from_start_of_array(array)
