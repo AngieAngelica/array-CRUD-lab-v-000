4dcf3cc89283e442333cef4f9c8bc08b2dc2b57f
Angie Kassab <anasab12@gmail.com>
2018-07-03 16:13:47 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  emotions = ["happy","happy","joy","joy"]
+  array = ["wow", "I", "am", "really", "learning"]
   emotions << "arrays!"
 end
 
