e2b9213d24fb7132aceb1f4e95f55b26921d6e1a
Angie Kassab <anasab12@gmail.com>
2018-07-03 15:55:21 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,8 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   emotions = ["happy","happy","joy","joy"]
-  
-  
+  emotions <<"content"
 end
 
 def add_element_to_start_of_array(array, element)
