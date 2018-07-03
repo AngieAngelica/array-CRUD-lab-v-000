90bd6d12cce5192d8a9910b15f5f01982391d810
Angie Kassab <anasab12@gmail.com>
2018-07-03 16:15:31 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,8 +17,8 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  emotions = ["happy","happy","joy","joy","arrays"]
-  emotions.pop
+  array = ["I", "am", "really", "learning", "arrays!"]
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
