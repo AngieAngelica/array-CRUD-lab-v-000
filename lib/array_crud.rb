13fc75727b1a37d0e374f4310a390782824f827d
Angie Kassab <anasab12@gmail.com>
2018-07-03 15:57:56 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,7 +12,8 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  emotions = ["happy","happy","joy","joy"]
+  emotions << "arrays!"
 end
 
 def remove_element_from_end_of_array(array)
